d02f202f6e71963475228a45a4f88e459dc66cb5
unknown <trietdang96@gmail.com>
2020-11-16 23:10:23 +0700

    create back end project

---
+++ src/main/java/com/iw/iwbackend/IwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iw.iwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iw/iwbackend/IwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iw.iwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
